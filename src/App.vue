5cff3f8f69ea9ae96ab9845958b47ccaeb93bb92
jenniferslagt <jennifer98_s@live.nl>
2020-09-28 16:01:35 +0200

    splitted app in components

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,43 @@
 <template>
   <div id="app" v-bind:class="typeof weather.main != 'undefined' && weather.main.temp > 17 ? 'warm' : ''">
-    <main> 
-      <div class="search-box">
-          <input type="text" class="search-bar" placeholder="Search..." v-model="query" @keypress="fetchWeather"/>
-      </div>
+    <main>
+     <SearchBox @search="fetchWeather($event)" />
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
-        <div class="location-box">
-          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
-          <div class="date">{{ dateBuilder() }}</div>
-        </div>
-
-        <div class="weather-box">
-          <div class="temp">{{ Math.round(weather.main.temp) }} °C </div> 
-          <div class="weather">{{weather.weather[0].main}}</div>
-        </div>
+        <LocationBox v-bind:weather="weather"/>
+        <WeatherBox v-bind:weather="weather"/>
       </div>
     </main>
   </div>
 </template>
 
 <script>
+import SearchBox from "./components/SearchForm";
+import LocationBox from "./components/LocationBox";
+import WeatherBox from "./components/WeatherBox";
 
 export default {
   name: 'App',
-  // components: {
-  // }
+  components: {
+    SearchBox,
+    LocationBox,
+    WeatherBox
+  },
   data () {
     return {
       api_key: '327256d3ecc9391886aea340632ab22f',
       url_base: 'https://api.openweathermap.org/data/2.5/',
-      query: '',
       weather: {} 
     }
   },
   methods: {
-    fetchWeather(e) {
-      if(e.key == "Enter") {
-        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+    fetchWeather(query) {   
+        fetch(`${this.url_base}weather?q=${query}&units=metric&APPID=${this.api_key}`)
         .then(res => {
           return res.json();
         }).then(this.setResults)
-      }
     },
     setResults (results) {
       this.weather = results; 
-    },
-    dateBuilder() {
-      let d = new Date();
-      let months = ["January", "Febraury", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-      let day = days[d.getDay()];
-      let date = d.getDate();
-      let month = months[d.getMonth()];
-      let year = d.getFullYear();
-
-      return `${day} ${date} ${month} ${year}`
     }
   }
 }
@@ -88,74 +70,5 @@ export default {
     padding: 25px;
     background: rgb(2,0,36);
     background: linear-gradient(0deg, rgba(2,0,36,0.2) 0%, rgba(4,4,56,0.2) 35%); 
-  }
-
-  .search-box {
-    width: 100%;
-    margin-bottom: 30px;
-  }
-
-  .search-bar {
-    display: block;
-    width: 100%;
-    padding: 15px;
-    color: black;
-    font-size: 20px;
-    appearance: none;
-    border: none;
-    outline: none;
-    box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
-    background: none;
-    background-color: rgba(255, 255, 255, 0.671);
-    border-radius: 0px 16px 0px 16px;
-    transition: 0.4s;
-  }
-
- .search-bar:focus {
-    background-color: rgb(255, 255, 255);
-    box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
-    border-radius: 0px 16px 0px 16px;
-  }
-
-  .location-box .location {
-    color: #fff;
-    font-size: 32px;
-    font-weight: 500;
-    text-align: center;
-    text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
-  }
-
-  .location-box .date {
-    color: #fff;
-    font-size: 20px;
-    font-weight: 300;
-    font-style: italic;
-    text-align: center;
-  }
-
-  .weather-box {
-    text-align: center;
-  }
-
-  .weather-box .temp {
-    display: inline-block;
-    padding: 10px 25px;
-    color: white;
-    font-size: 102px;
-    font-weight: 900;
-    text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
-    background-color: rgba(255, 255, 255, 0.25);
-    border-radius: 16px;
-    margin: 30px 0px;
-    box-shadow: 3px 6px rgba(0, 0, 0, 0.25)
-  }
-
-  .weather-box .weather {
-    color: white;
-    font-size: 48px;
-    font-weight: 700;
-    font-style: italic;
-    text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
-
   }
 </style>
---
+++ src/components/LocationBox.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="location-box">
+        <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+        <div class="date">{{ dateBuilder() }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "LocationBox",
+     props: {
+        weather: Object
+    },
+    methods: {
+    dateBuilder() {
+      let d = new Date();
+      let months = ["January", "Febraury", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day} ${date} ${month} ${year}`
+    }
+    }
+}
+</script>
+
+<style scoped>
+.location-box .location {
+    color: #fff;
+    font-size: 32px;
+    font-weight: 500;
+    text-align: center;
+    text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+  }
+
+  .location-box .date {
+    color: #fff;
+    font-size: 20px;
+    font-weight: 300;
+    font-style: italic;
+    text-align: center;
+  }
+</style>
